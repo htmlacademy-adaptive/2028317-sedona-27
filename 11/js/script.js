9cb3604b567ea5c1738def37d31c1683e1f6a0c7
Keks <keks@htmlacademy.ru>
2023-01-25 01:12:14 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/script.js
+++ 11/js/script.js
@@ -1,1 +1,17 @@
-let navMain=document.querySelector(".burger-menu"),navToggle=document.querySelector(".burger-menu__toggle");console.log(navMain),console.log(navToggle),navMain.classList.remove("burger-menu--nojs"),navToggle.addEventListener("click",(function(){navMain.classList.contains("burger-menu--closed")?(navMain.classList.remove("burger-menu--closed"),navMain.classList.add("burger-menu--opened")):(navMain.classList.add("burger-menu--closed"),navMain.classList.remove("burger-menu--opened"))}));
+let navMain = document.querySelector('.burger-menu');
+let navToggle = document.querySelector('.burger-menu__toggle');
+
+console.log(navMain);
+console.log(navToggle);
+
+navMain.classList.remove('burger-menu--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('burger-menu--closed')) {
+    navMain.classList.remove('burger-menu--closed');
+    navMain.classList.add('burger-menu--opened');
+  } else {
+    navMain.classList.add('burger-menu--closed');
+    navMain.classList.remove('burger-menu--opened');
+  }
+});
